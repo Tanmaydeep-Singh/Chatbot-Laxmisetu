f6391d4c372964029795327fa735fd71da782269
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2025-01-11 14:35:21 +0530

    dev: CORS handel

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from utils.chatbot_utils import get_chatbot_response
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
